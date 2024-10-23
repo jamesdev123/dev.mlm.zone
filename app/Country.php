f1d29ad305edf62303afd3bb1ef79e3932242568
dreamdev <sammimak2@gmail.com>
2024-10-22 17:29:33 -0700

    modify country model

---
+++ app/Country.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+
+class Country
+{
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'zipcode','shortname',
+    ];
+
+}
