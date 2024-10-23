3b38a613c36a02fc875bba14e84d54b5b926fed0
dreamdev <sammimak2@gmail.com>
2024-10-22 17:05:15 -0700

    C:/Program Files/Git/get/country bug fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,4 +25,9 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
+    public function getCountry(){
+        $data = array("USA", "Russia");
+        return json_encode($data);
+    }
 }
